37824f94c01d8785174630b10883bd7c86e19478
yant1001 <eunseo.yang1001@gmail.com>
2024-07-17 21:50:57 +0900

    upload dataset2 for sql practice

--- SQL_실습/상품 리뷰 데이터를 이용한 리포트 작성.sql
+++ SQL_실습/상품 리뷰 데이터를 이용한 리포트 작성.sql
@@ -1,4 +1,16 @@
 USE mydata;
 SELECT * FROM dataset2;
 
-select * from dataset3;
+-- Division Name(상품이 속한 division)별 평균 평점
+SELECT 'DIVISION NAME',
+	   AVG(Rating) AS AVG_RATE
+FROM mydata.dataset2
+GROUP BY 1
+ORDER BY 2 DESC;
+
+-- Department(상품이 속한 department)별 평균 평점
+SELECT 'DEPARTMENT NAME',
+	   AVG(Rating) AS AVG_RATE
+FROM mydata.dataset2
+GROUP BY 1
+ORDER BY 2 DESC;
